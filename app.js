c90f7e85642545ce60ea16b251df95cbf10becf5
Chase Gruber <chasegruber@gmail.com>
2015-12-29 20:05:29 -0500

    OOJS viz

--- app.js
+++ app.js
@@ -1,43 +1,72 @@
-var svg = d3.select("body").append("svg").attr("height",800).attr("width",900);
-d3.csv("OE.csv",function(data){
-  makeChart(data);
-  d3.select("select").on("change",function(d){
-    var oeDomain = d3.extent(data, function(d){return +d["Avg. Oe"]})
-    var oeScale = d3.scale.linear().domain(oeDomain).range([0,900]);
-    transitionChart(getField(),makeYScale(data,getField()),oeScale);
-  })
-})
-
-function getField(){
-  return d3.select("select").node().value;
-}
-
-function makeYScale(data,field){
-  var yDomain = d3.extent(data,function(d){return +d[field]});
-  var yScale = d3.scale.linear().domain(yDomain).range([800,0]);
-  return yScale;
-}
-
-function transitionChart(field,yScale,oeScale){
-  svg.selectAll("circle").transition().duration(1000)
-    .attr("transform",function(d){
-      return "translate("+oeScale(d["Avg. Oe"])+","+yScale(d[field])+")";
+var oeViz = {
+  svgAtt:{
+    width:parseFloat(d3.select('.svgContain').style('width')),
+    height:parseFloat(d3.select('.svgContain').style('height')),
+    margins:{
+      left:60,
+      right:50,
+      top:20,
+      bottom:50
+    }
+  },
+  page:{
+    scales:{}
+  },
+  getPageComponents:function(){
+    this.page.svg = d3.select(".svgContain").append("svg")
+      .attr("height",this.svgAtt.height).attr("width",this.svgAtt.width);
+  },
+  getField:function(){
+    return d3.select("select").node().value;
+  },
+  makeScale:function(field, data, range){
+    var domain = d3.extent(data,function(d){return +d[field]});
+    return d3.scale.linear().domain(domain).range(range)
+  },
+  makeScales:function(data){
+    var fields = ["Avg. Assists","Avg. Points","Avg. Field Goals Attempted","Avg. Field Goals Made","Avg. Rebounds Offensive","Avg. Rebounds Total","Avg. Turnovers"];
+    for (var i=0;i<fields.length;i++){
+      var field = fields[i];
+      this.page.scales[field] = this.makeScale(field,data,[this.svgAtt.height,0])
+    }
+    this.page.scales["Avg. Oe"] = this.makeScale(field,data,[0,this.svgAtt.width])
+  },
+  makeChart:function(data){
+    var self = this;
+    var field = this.getField();
+    this.page.svg.selectAll(".player").data(data).enter().append("circle")
+      .attr("transform",function(d){
+        return "translate("+self.page.scales["Avg. Oe"](d["Avg. Oe"])+","+self.page.scales[field](d[field])+")"
+      })
+      .attr("r",6)
+      .attr("fill","none")
+      .attr("stroke",function(d){
+        return (d["Pos. Type"]=="big")?"red":"blue";
+      })
+  },
+  transitionChart:function(field){
+    var self = this;
+    this.page.svg.selectAll("circle").transition().duration(1000)
+      .attr("transform",function(d){
+        return "translate("+self.page.scales["Avg. Oe"](d["Avg. Oe"])+","+self.page.scales[field](d[field])+")"
+      })
+  },
+  bindInteraction:function(){
+    var self = this;
+    d3.select("select").on("change",function(){
+      var field = self.getField();
+      self.transitionChart(field);
     })
-}
-
-function makeChart(data){
-  var field = getField();
-  var oeDomain = d3.extent(data, function(d){return +d["Avg. Oe"]})
-  var oeScale = d3.scale.linear().domain(oeDomain).range([0,900]);
-  var yDomain = d3.extent(data,function(d){return +d[field]});
-  var yScale = d3.scale.linear().domain(yDomain).range([800,0]);
-  svg.selectAll(".player").data(data).enter().append("circle")
-    .attr("transform",function(d){
-      return "translate("+oeScale(d["Avg. Oe"])+","+yScale(d[field])+")"
+  },
+  init:function(){
+    this.getPageComponents();
+    var self = this;
+    d3.csv("OE.csv",function(data){
+      self.page.data = data;
+      self.makeScales(data);
+      self.makeChart(data);
+      self.bindInteraction();
     })
-    .attr("r",6)
-    .attr("fill","none")
-    .attr("stroke",function(d){
-      return (d["Pos. Type"]=="big")?"red":"blue";
-    })
+  }
 }
+oeViz.init();
