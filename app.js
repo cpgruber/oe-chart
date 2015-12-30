3f0189758619c01795090299e4c6b75b23530d0e
Chase Gruber <chasegruber@gmail.com>
2015-12-29 19:21:38 -0500

    scale y-axis using user input

--- app.js
+++ app.js
@@ -2,11 +2,42 @@ var svg = d3.select("body").append("svg").attr("height",800).attr("width",900);
 d3.csv("OE.csv",function(data){
   makeChart(data);
   d3.select("select").on("change",function(d){
-    console.log(d3.select(this).node().value)
+    var oeDomain = d3.extent(data, function(d){return +d["Avg. Oe"]})
+    var oeScale = d3.scale.linear().domain(oeDomain).range([0,900]);
+    transitionChart(getField(),makeYScale(data,getField()),oeScale);
   })
 })
 
+function getField(){
+  return d3.select("select").node().value;
+}
+
+function makeYScale(data,field){
+  var yDomain = d3.extent(data,function(d){return +d[field]});
+  var yScale = d3.scale.linear().domain(yDomain).range([800,0]);
+  return yScale;
+}
+
+function transitionChart(field,yScale,oeScale){
+  svg.selectAll("circle").transition().duration(1000)
+    .attr("transform",function(d){
+      return "translate("+oeScale(d["Avg. Oe"])+","+yScale(d[field])+")";
+    })
+}
+
 function makeChart(data){
+  var field = getField();
   var oeDomain = d3.extent(data, function(d){return +d["Avg. Oe"]})
   var oeScale = d3.scale.linear().domain(oeDomain).range([0,900]);
+  var yDomain = d3.extent(data,function(d){return +d[field]});
+  var yScale = d3.scale.linear().domain(yDomain).range([800,0]);
+  svg.selectAll(".player").data(data).enter().append("circle")
+    .attr("transform",function(d){
+      return "translate("+oeScale(d["Avg. Oe"])+","+yScale(d[field])+")"
+    })
+    .attr("r",6)
+    .attr("fill","none")
+    .attr("stroke",function(d){
+      return (d["Pos. Type"]=="big")?"red":"blue";
+    })
 }
